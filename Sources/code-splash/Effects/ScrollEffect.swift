137d1c1930979fdaf076696ef2e6332ec2222fc8
Haruka Kataoka <hrk8@drumsoft.com>
2025-10-09 02:52:02 +0900

    Added color fading to ScrollEffect

--- Sources/code-splash/Effects/ScrollEffect.swift
+++ Sources/code-splash/Effects/ScrollEffect.swift
@@ -29,13 +29,15 @@ class ScrollEffect: VisualEffect {
             alpha: 1.0
         )
         let scrollUp = Float.random(in: 0..<1) < 0.8
-        let vy: CGFloat = (scrollUp ? 1 : -1) * max(3000, (textField.frame.height + contentView.frame.height) / 1.0)
+        let vy: CGFloat = (scrollUp ? 1 : -1) * max(2500, (textField.frame.height + contentView.frame.height) / 1.5)
         textField.frame.origin = CGPoint(
             x: contentView.bounds.minX + contentView.bounds.width * CGFloat.random(in: 0.05...0.5),
             y: scrollUp ? contentView.bounds.minY - textField.frame.height : contentView.bounds.maxY
         )
         containerView.addSubview(textField)
-        animative = Animative(textField, bounds: contentView.bounds, vy: vy)
+        animative = Animative(
+            textField, bounds: contentView.bounds, colorIndexPair: Colors.shared.colorIndexPair(),
+            vy: vy)
 
         Timer.scheduledTimer(withTimeInterval: 1.0 / 60.0, repeats: true) { timer in
             let elapsed = CACurrentMediaTime() - startTime
@@ -55,22 +57,27 @@ class ScrollEffect: VisualEffect {
     private struct Animative {
         let view: NSTextField
         let bounds: CGRect
+        let colorIndexPair: (Int, Int)
         var vy: CGFloat
 
         init(
-            _ view: NSTextField, bounds: CGRect, vy: CGFloat
+            _ view: NSTextField, bounds: CGRect, colorIndexPair: (Int, Int), vy: CGFloat
         ) {
             self.view = view
             self.bounds = bounds
+            self.colorIndexPair = colorIndexPair
             self.vy = vy
         }
 
         func animate(elapsed: Double) -> Bool {
             let y: CGFloat = vy * elapsed;
+            let ratio = max(0, min(1, abs(y) / (bounds.height + view.frame.height)))
 
             var transform: CATransform3D = CATransform3DIdentity
             transform = CATransform3DTranslate(transform, 0, y, 0)
             view.layer?.transform = transform
+            view.textColor = Colors.shared.gradientColor(
+                at: ratio, between: colorIndexPair)
 
             return (view.frame.minY + y < bounds.maxY || vy < 0)
                 && (view.frame.maxY + y > bounds.minY || vy > 0);
